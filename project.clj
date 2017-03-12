7cf1befd41aa8f2aeb5fc93cb2ca41afcf5a89d4
Jarppe <jarppe@metosin.fi>
2017-03-12 17:10:59 +0200

    Updated deps, added logging.

--- project.clj
+++ project.clj
@@ -1,44 +1,48 @@
 (defproject bootcamp3 "0.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
 
                  ; Common libs:
-                 [prismatic/schema "0.4.0"]
-                 [prismatic/plumbing "0.4.2"]
-                 [metosin/potpuri "0.2.2"]
+                 [prismatic/schema "1.1.3"]
+                 [prismatic/plumbing "0.5.3"]
+                 [metosin/potpuri "0.4.0"]
 
                  ; Ring:
-                 [ring/ring "1.3.2" :exclusions [org.clojure/tools.reader clj-time commons-codec]]
-                 [ring/ring-defaults "0.1.4"]
+                 [ring/ring-core "1.5.1"]
+                 [ring/ring-defaults "0.2.3"]
+                 [ring/ring-jetty-adapter "1.5.1"]
 
                  ; Rest API:
-                 [metosin/compojure-api "0.19.3"]
-                 [metosin/ring-http-response "0.6.1" :exclusions [ring/ring-core]]
+                 [metosin/compojure-api "1.1.10"]
+                 [metosin/ring-http-response "0.8.2"]
 
                  ; Swagger UI
-                 [metosin/ring-swagger-ui "2.1.0-M2-2"]
+                 [metosin/ring-swagger-ui "2.2.8"]
 
                  ; HTTP and HTML
-                 [clj-http "1.1.1"]
+                 [clj-http "3.4.1"]
                  [hiccup "1.0.5"]
-                 [enlive "1.1.5"]
+                 [enlive "1.1.6"]
 
                  ; Async:
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.async "0.3.441"]
 
                  ; ClojureScript:
-                 [org.clojure/tools.reader "0.9.2"]
-                 [org.clojure/clojurescript "0.0-3211"]
-                 [prismatic/dommy "1.0.0"]]
+                 [org.clojure/clojurescript "1.9.456"]
+                 [org.clojure/tools.reader "1.0.0-beta4"]
+                 [prismatic/dommy "1.1.0"]
+
+                 ; Logging:
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/jcl-over-slf4j "1.7.24"]
+                 [org.slf4j/jul-to-slf4j "1.7.24"]
+                 [org.slf4j/log4j-over-slf4j "1.7.24"]
+                 [ch.qos.logback/logback-classic "1.2.1" :exclusions [org.slf4j/slf4j-api]]]
 
   :source-paths ["src" "cljs-src"]
 
-  :profiles {:dev {:dependencies [[flare "0.2.8"]
-                                  [ring-mock "0.1.5"]]
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]
                    :resource-paths ["target/generated"]
-                   :injections [(require 'flare.clojure-test)
-                                (flare.clojure-test/install!)]
-                   :plugins [[lein2-eclipse "2.0.0" :exclusions [org.clojure/clojure]]
-                             [lein-cljsbuild "1.0.5"]]}
+                   :plugins [[lein-cljsbuild "1.0.5"]]}
              :uberjar {:main  bootcamp.main
                        :aot   [bootcamp.main]
                        :uberjar-name "bootcamp.jar"}}
